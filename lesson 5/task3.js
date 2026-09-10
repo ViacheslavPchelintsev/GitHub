774d5840af7720d00bff04324c7af5c0bab6c529
ViacheslavPchelintsev <leon050492@gmail.com>
2026-09-11 01:33:19 +0200

    lesoong 5 ready

---
+++ lesson 5/task3.js
@@ -0,0 +1,15 @@
+const number = 6;
+
+for (let i = 1; i < 11; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
+
+console.log("------------");
+
+const number2 = 3;
+let i2 = 1;
+
+while (i2 <= 10) {
+    console.log(`${number2} * ${i2} = ${number2 * i2}`)
+    i2++;
+}
